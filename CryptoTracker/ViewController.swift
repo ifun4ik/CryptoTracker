9c5510931c9dbae78adda68ab63d98ccc6db0e35
Gl1n- <funnydemscreation@gmail.com>
2019-06-09 10:43:35 +0300

    Added TableView

--- CryptoTracker/ViewController.swift
+++ CryptoTracker/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+	
+	var cryptoAb = [String]()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		let justData = "BTC  ETH  XYU  XER  UAH  RUB  USD"
+		cryptoAb = justData.components(separatedBy: String("  "))
 	}
-
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return cryptoAb.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+		cell?.textLabel?.text = cryptoAb[indexPath.row]
+		return cell!
+	}
 
 }
 
